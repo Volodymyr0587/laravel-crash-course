97d72c432ffe1b501281068fbad20cd66df70cf4
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-27 22:47:26 +0300

    06 - Relationships for post and categories

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,6 +1,6 @@
 <div class="p-10 mb-4">
     <div>
-        <a href="" class="text-blue-500">Post category</a>
+        <a href="#" class="text-blue-500">{{ $post->category['title'] }}</a>
     </div>
     <div>
         <p>{{ $post->title }}</p>
@@ -9,7 +9,7 @@
         <p>{{ $post->description }}</p>
     </div>
     <div class="mt-4">
-        <a href="" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read
+        <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Read
             more...</a>
     </div>
 </div>
